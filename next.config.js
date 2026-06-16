74801edbaff7adb34ee34107ff9b9a3b0afc45f4
Masroor Khan <makk3435@gmail.com>
2026-06-16 23:06:56 +0530

    Fix next.config.js outputFileTracingRoot for Vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
   productionBrowserSourceMaps: false,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
